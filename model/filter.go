11689fec44542516fffe49cbe4374135d70677dd
suyh1988 <30858673+suyh1988@users.noreply.github.com>
2024-11-29 10:12:47 +0800

    Delete model directory

--- model/filter.go
+++
@@ -1,9 +0,0 @@
-package model
-
-type Filter struct {
-	InFile             string //filter input mysqldump file
-	OutFile            string //filtered mysqldump file
-	TableList          string //filter databases of mysqldump file
-	BufferSize         int
-	LinePrefixLongByte int
-}
